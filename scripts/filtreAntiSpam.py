7ab6da136f39825d9c9f1e02aa441a8e46d134ae
Jofrey Luc <luc.jofrey@yahoo.fr>
2017-04-07 15:08:02 +0200

    alpha des fonctions

--- scripts/filtreAntiSpam.py
+++ scripts/filtreAntiSpam.py
@@ -1,1 +1,19 @@
-#
+import numpy as np
+from glob import glob
+
+def charger_dictionnaire() :
+    1+1
+
+def lire_message() :
+    1+1
+
+def apprendre_message(dicoProbas, message, etiquette) :
+    vecteurPresence = lire_message(message)
+    
+
+def apprendre_base(cheminDico, dossierSpam, dossierHam) :
+    dicoProbas = charger_dictionnaire(cheminDico)
+    for (m in glob(dossierSpam + '/*.txt')) : apprendre_message(dicoProbas, m, 'SPAM')
+    for (m in glob(dossierHam + '/*.txt')) : apprendre_message(dicoProbas, m, 'HAM')
+
+    
