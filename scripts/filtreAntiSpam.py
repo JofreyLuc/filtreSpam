ad9913d0c28def1ca5bd609eb531b275a8d1e9db
Zhephyr54 <ale.lanoix@gmail.com>
2017-04-07 15:58:16 +0200

    fonction charger dictionnaire et liremessage

--- scripts/filtreAntiSpam.py
+++ scripts/filtreAntiSpam.py
@@ -1,11 +1,25 @@
 import numpy as np
 from glob import glob
+import regex as re 
 
-def charger_dictionnaire() :
-    1+1
-
-def lire_message() :
-    1+1
+def charger_dictionnaire(dicoFilePath, minNbOfChar=3) :
+    dico = {}
+    with open(dicoFilePath, 'r') as f :
+        for word in f :
+            word = ''.join(word.split())    # Enlève les éventuels whitespace
+            if len(word) < minNbOfChar:     # Si le mot est de longueur minimale
+                dico[word.upper()] = False  # On capitalise le mot et on le place en clef du dico
+    return dico
+    
+def lire_message(messageFilePath, dico) :
+    with open(messageFilePath, 'r') as contenu:
+        content = content_file.read()
+        messageWords = re.split('\W+', content)
+        # On parcourt les mots présents dans le mail
+        for word in messageWords :
+            if word.upper() in dico :
+                dico[word.upper()] = True
+     return dico
 
 def apprendre_message(dicoProbas, message, etiquette) :
     vecteurPresence = lire_message(message)
