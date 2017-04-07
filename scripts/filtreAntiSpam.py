6e70c39567a58d3b4359da728b3b1300117e358e
Jofrey Luc <luc.jofrey@yahoo.fr>
2017-04-07 15:57:38 +0200

    apprentissage + prediction

--- scripts/filtreAntiSpam.py
+++ scripts/filtreAntiSpam.py
@@ -7,13 +7,39 @@ def charger_dictionnaire() :
 def lire_message() :
     1+1
 
-def apprendre_message(dicoProbas, message, etiquette) :
+def apprendre_ham(dicoProbas, message, nbHam) :
     vecteurPresence = lire_message(message)
-    
+    for (mot : vecteurPresence) :
+        ancienneValeur = dicoProbas[mot][1]
+        ancienneValeur *= nbHam
+        ancienneValeur += 1
+        ancienneValeur /= nbHam
+        dicoProbas[mot][1] = ancienneValeur
 
+def apprendre_spam(dicoProbas, message, nbSpam) :
+    vecteurPresence = lire_message(message)
+    for (mot : vecteurPresence) :
+        ancienneValeur = dicoProbas[mot][0]
+        ancienneValeur *= nbSpam
+        ancienneValeur += 1
+        ancienneValeur /= nbSpam
+        dicoProbas[mot][0] = ancienneValeur
+    
 def apprendre_base(cheminDico, dossierSpam, dossierHam) :
     dicoProbas = charger_dictionnaire(cheminDico)
     for (m in glob(dossierSpam + '/*.txt')) : apprendre_message(dicoProbas, m, 'SPAM')
     for (m in glob(dossierHam + '/*.txt')) : apprendre_message(dicoProbas, m, 'HAM')
 
+def predire_message(cheminMessage, nbSpam, nbHam, dicoProbas) :
+    vecteurPresence = lire_message(cheminMessage)
+    Pspam = nbSpam/(nbSpam+nbHam)
+    Pham = nbHam/(nbSpam+nbHam)
+    #PASSAGE EN LOG
+    for (j in dicoProbas) :
+        if (j in vecteurPresence) :
+            Pspam *= dicoProbas[j][0]
+            Pham *= dicoProbas[j][1]
+        else :
+            Pspam *= (1-dicoProbas[j][0])
+            Pham *= (1-dicoProbas[j][1])
     
